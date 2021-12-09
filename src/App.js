a72a5f1eb5ee0537053a7c69876ba1ac3fa63ee6
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2021-12-09 13:05:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "./firebase/firebaseConfig";
+import { BsSearch } from 'react-icons/bs';
+import './styles/App.css'
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [list, setList] = useState('');
+
+  //Obtener la data
+  useEffect(() => {
+    const getDatos = async () => {
+      const datos = await getDocs(collection(db, "products"));
+      setProducts(datos.docs.map((obj) => obj.data()));
+    };
+
+    getDatos();
+  }, []);
+
+  //Listas organizadas por precios
+  useEffect(() => {
+    if (list === "Menor a mayor precio") {
+      const newSortedList = [...products].sort((a, b) => a.Precio - b.Precio);
+      setProducts(newSortedList);
+    } else if (list === "Mayor a menor precio") {
+      const newSortedList = [...products].sort((a, b) => b.Precio - a.Precio);
+      setProducts(newSortedList);
+    }
+  }, [list]);
+
+  function handleSelect(e) {
+    setList(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fashion shop</h1>
+      <div className="header">
+      <div className="input">
+      <input className="inputSearch" placeholder="Busca tu producto" /><BsSearch className="logoInput"/>
+      </div>
+      <select className="selector" value={list} onChange={handleSelect}>
+        <option>Menor a mayor precio</option>
+        <option>Mayor a menor precio</option>
+      </select>
+      </div>
+      {products.map((obj) => (
+        <div className="products" key={obj.Id}>
+          <img className="imgProducts" src={obj.Img} alt="" />
+          <h2 className="titleProducts">{obj.Nombre}</h2>
+          <p className="priceProducts">${obj.Precio}</p>
+          <button className="buttonProducts">Comprar</button>
+        </div>
+      ))}
     </div>
   );
 }
